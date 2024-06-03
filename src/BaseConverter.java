152c21af20c560c44eb5f34d25dea1c888938671
ScreamyL <evklid999@gmail.com>
2024-06-03 18:43:54 +0300

    норм код

--- src/BaseConverter.java
+++ src/BaseConverter.java
@@ -1,15 +1,35 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class BaseConverter {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите температуру в градусах Цельсия: ");
+        double celsius = scanner.nextDouble();
+
+        System.out.println("Конвертировать в:");
+        System.out.println("1.Кельвины");
+        System.out.println("2.Фаренгейты");
+        int choice = scanner.nextInt();
+
+        BaseConverter converter = new BaseConverter();
+        converter.convert(celsius, choice);
+
+        scanner.close();
+    }
+
+    public void convert(double celsius, int choice) {
+        switch (choice) {
+            case 1:
+                double kelvin = celsius + 273.15;
+                System.out.println(celsius + " градусов Цельсия = " + kelvin + " Кельвин");
+                break;
+            case 2:
+                double fahrenheit = celsius * 9 / 5 + 32;
+                System.out.println(celsius + " градусов Цельсия = " + fahrenheit + " Фаренгейт");
+                break;
+            default:
+                System.out.println("Некорректный выбор");
         }
     }
 }
